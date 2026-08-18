cd20069abd4523db82fb9f606fe696db39cd1413
Achint Kaur Gill <achintkaurgill@gmail.com>
2026-08-18 10:54:11 +0530

    Add files via upload

---
+++ createurowntuple.py
@@ -0,0 +1,4 @@
+my_tuple=['hi', 'my', 'name', 'was', 'she']
+print(my_tuple)
+print("The first element of the tuple is: ", my_tuple[0])
+print("Last element: ", my_tuple[4])
---
+++ occuranceofanelement.py
@@ -0,0 +1,4 @@
+#program to count the occurance of an element in a list
+my_1=[12,34,12,34,45,45,342,32]
+count=0
+print(my_1.count(12))
