b0917f71d878c45a324a5ab6383152a7604a45e3
Achint Kaur Gill <achintkaurgill@gmail.com>
2026-08-19 13:06:33 +0530

    Add files via upload

---
+++ listofnumbersdivisibleby5.py
@@ -0,0 +1,4 @@
+#to create a list of all numbers between 1-1000 divisible by 5
+num_divisible_by_5=list(range(5,1001,5))
+print("There are ",len(num_divisible_by_5), " divisible by 5.")
+print("The numbers that are divisible by 5 are: ",num_divisible_by_5)
