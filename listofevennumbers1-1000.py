ac822548200d706fd0ce010ef0790762240314cb
Achint Kaur Gill <achintkaurgill@gmail.com>
2026-08-19 12:51:45 +0530

    Add files via upload

---
+++ listofevennumbers1-1000.py
@@ -0,0 +1,4 @@
+#to create a list of even numbers between 1-1000
+even_numbers=list(range(2,1001,2))
+print("The even numbers are: ",even_numbers)
+print("The number of even numbers from 1-1000 are: ", len(even_numbers))
