bb9eb497b747d8696196a7d36b2b9a1ab05e57ea
Achint Kaur Gill <achintkaurgill@gmail.com>
2026-08-19 12:53:13 +0530

    Add files via upload

---
+++ listofoddnumbers1-1000.py
@@ -0,0 +1,4 @@
+#to create a list of odd numbers between 1-1000
+odd_numbers=list(range(1,1001,2))
+print("The odd numbers are: ",odd_numbers)
+print("The number of odd numbers from 1-1000 are: ", len(odd_numbers))
